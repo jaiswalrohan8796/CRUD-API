afb467d11e8b64ca4d1abf27a252fed98f8eae2a
Rohan Jaiswal <jaiswalrohan8796@gmail.com>
2020-12-30 14:10:00 +0530

    config

--- app.js
+++ app.js
@@ -2,15 +2,14 @@ import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import router from "./routes/route.js";
-
+import mongodbURI from './config.js'
 const app = express();
 
 //using json data format
 app.use(bodyParser.json(true));
 
 mongoose
-    .connect(
-        "mongodb://jaiswalrohan:jaiswalrohan@cluster0-shard-00-00.wq1cd.mongodb.net:27017,cluster0-shard-00-01.wq1cd.mongodb.net:27017,cluster0-shard-00-02.wq1cd.mongodb.net:27017/Cluster0?ssl=true&replicaSet=atlas-yf9ewr-shard-0&authSource=admin&retryWrites=true&w=majority",
+    .connect( mongodbURI,
         { useNewUrlParser: true,  useUnifiedTopology: true }
     )
     .then(() => {
---
+++ config.js
@@ -0,0 +1,3 @@
+ const mongodbURI = "mongodb://jaiswalrohan:jaiswalrohan@cluster0-shard-00-00.wq1cd.mongodb.net:27017,cluster0-shard-00-01.wq1cd.mongodb.net:27017,cluster0-shard-00-02.wq1cd.mongodb.net:27017/Cluster0?ssl=true&replicaSet=atlas-yf9ewr-shard-0&authSource=admin&retryWrites=true&w=majority"
+
+ export default mongodbURI;
